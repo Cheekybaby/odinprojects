0df3a85292ee364adaef7eedc6f5191a3df4f785
Shashank Ranjan <shashankranjan07@gmail.com>
2024-07-08 22:28:59 +0530

    ui works

--- rock-paper-scissor/script.js
+++ rock-paper-scissor/script.js
@@ -11,40 +11,7 @@ function getcomputerinput(computer) {
     }
 }
 
-function gethumaninput(player) {
-    player = prompt("ENTER YOUR CHOICE: ");
+function playround() {
 
-    return player.toUpperCase();
 }
 
-function game(player, computer) {
-    let scoreP = 0, scoreC = 0;
-    for (i=0; i<5; i+=1){
-        player = gethumaninput();
-        computer = getcomputerinput();
-
-        if (player === computer){
-            alert(`TIE!!
-                Player : Computer :: ${ scoreP } : ${ scoreC }`);
-        } else if ((player === "ROCK" && computer === "SCISSOR") || (player === "PAPER" && computer === "ROCK") || (player === "SCISSOR" && computer === "PAPER")){
-            scoreP+=1;
-            alert(`Player : Computer :: ${scoreP} : ${scoreC}`);
-        } else {
-            scoreC+=1;
-            alert(`Player : Computer :: ${scoreP} : ${scoreC}`);
-        }
-
-        if (scoreP == 3 || scoreC == 3){
-            break;
-        }
-    }
-
-    if (scoreP == 3 || scoreP > scoreC){
-        alert("You Win!!!");
-    } else if (scoreC == 3 || scoreC > scoreP){
-        alert("You Lose!!!");
-    } else {
-        alert("It's a TIE!!!");
-    }
-}
-game();
